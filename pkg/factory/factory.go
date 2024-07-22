70a19d8add99387f945edf5b7fc8cc803dbb60d1
idanilchenko@altsoft.biz <dobrogovremeny@gmail.com>
2024-07-22 14:23:06 +0300

    prepare factory module

---
+++ pkg/factory/factory.go
@@ -0,0 +1,56 @@
+package fatcory
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Alias string
+
+type Config struct {
+	Addr           string
+	ReadTimeout    time.Duration
+	WriteTimeout   time.Duration
+	MaxHeaderBytes int
+}
+
+type ConfigAliases struct {
+	Aliases *map[Alias]*Config
+}
+
+type Factory struct {
+	ConfigAliases *ConfigAliases
+}
+
+func NewConfig(paths ...string) (*Config, error) {
+	return &Config{}, nil
+}
+
+func NewFactory(configAliases *ConfigAliases) *Factory {
+	factory := &Factory{
+		ConfigAliases: configAliases,
+	}
+	return factory
+}
+
+func (f *Factory) NewGinEngine(alias Alias) (*gin.Engine, error) {
+	return gin.Default(), nil
+}
+
+func (f *Factory) NewServer(alias Alias, ginEngine *gin.Engine) (*http.Server, error) {
+	config := (*f.ConfigAliases.Aliases)[alias]
+	if config == nil {
+		return nil, fmt.Errorf("alias: %s not found", alias)
+	}
+	server := &http.Server{
+		Addr:           config.Addr,
+		Handler:        ginEngine,
+		ReadTimeout:    config.ReadTimeout,
+		WriteTimeout:   config.WriteTimeout,
+		MaxHeaderBytes: config.MaxHeaderBytes,
+	}
+	return server, nil
+}

6a16ce5445419feb4acf09b989b4440828a1c37b
idanilchenko@altsoft.biz <dobrogovremeny@gmail.com>
2024-07-25 17:14:43 +0300

    prepare configuration

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/IvanDobriy/GinFxModule
 go 1.22.2
 
 require (
+	github.com/IvanDobriy/tag v0.0.1
 	github.com/gin-gonic/gin v1.10.0
 	go.uber.org/fx v1.22.1
 )
@@ -12,12 +13,14 @@ require (
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -25,6 +28,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.uber.org/dig v1.17.1 // indirect
@@ -36,5 +41,6 @@ require (
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/factory/factory.go
+++ pkg/factory/factory.go
@@ -3,8 +3,11 @@ package factory
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/IvanDobriy/tag/pkg/tag"
+	"github.com/IvanDobriy/tag/pkg/tag/cast"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,15 +20,89 @@ type Config struct {
 	MaxHeaderBytes int
 }
 
+func GetConfigFromMap(raw map[string]any) (*Config, error) {
+	address, err := tag.GetRequired("addr", raw, func(value any) (*string, error) {
+		r, e := cast.ToStringE(value)
+		return &r, e
+	})
+	if err != nil {
+		return nil, err
+	}
+	readTimeout, err := tag.GetRequired("readTimeout", raw, func(value any) (*time.Duration, error) {
+		r, e := cast.ToDurationE(value)
+		return &r, e
+	})
+	if err != nil {
+		return nil, err
+	}
+	writeTimeout, err := tag.GetRequired("writeTimeout", raw, func(value any) (*time.Duration, error) {
+		r, e := cast.ToDurationE(value)
+		return &r, e
+	})
+	if err != nil {
+		return nil, err
+	}
+	maxHeaderBytes, err := tag.GetRequired("maxHeaderBytes", raw, func(value any) (*int, error) {
+		r, e := cast.ToIntE(value)
+		return &r, e
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &Config{Addr: *address, ReadTimeout: *readTimeout, WriteTimeout: *writeTimeout, MaxHeaderBytes: *maxHeaderBytes}, nil
+}
+
 type ConfigAliases struct {
 	Aliases *map[Alias]*Config
+}
+
+func GetConfigAlasesFromMap(raw map[string]any) (*ConfigAliases, error) {
+	rawConfig, err := tag.GetRequired("gin", raw, func(value any) (*map[string]any, error) {
+		r, e := cast.ToStringMapE(value)
+		return &r, e
+	})
+	if err != nil {
+		return nil, err
+	}
+	aliases := map[Alias]*Config{}
+	for name, el := range *rawConfig {
+		rawAlias, err := cast.ToStringMapE(el)
+		if err != nil {
+			return nil, err
+		}
+		aliases[Alias(name)], err = GetConfigFromMap(rawAlias)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &ConfigAliases{Aliases: &aliases}, nil
 }
 
 type Factory struct {
 	ConfigAliases *ConfigAliases
 }
 
-func NewConfig(paths ...string) (*ConfigAliases, error) {
+func NewConfig(paths ...string) (ca *ConfigAliases, err error) {
+	if len(paths) == 0 {
+		return nil, fmt.Errorf("configuration file paths is empty")
+	}
+	var conf *os.File = nil
+	for _, path := range paths {
+		file, e := os.OpenFile(path, os.O_RDONLY, 0640)
+		if e == nil {
+			conf = file
+			break
+		}
+	}
+	if conf == nil {
+		return nil, fmt.Errorf("no config files found, paths: %v", paths)
+	}
+	defer func() {
+		if e := conf.Close(); e != nil {
+			err = e
+		}
+	}()
+
 	return &ConfigAliases{}, nil
 }
 
